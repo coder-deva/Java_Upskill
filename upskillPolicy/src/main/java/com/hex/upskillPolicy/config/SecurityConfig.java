0f5ee4060aac95d41ae6270b246ef1d5bfc22742
coder-deva <2116012@saec.ac.in>
2025-11-10 11:44:51 +0530

    config

--- upskillPolicy/src/main/java/com/hex/upskillPolicy/config/SecurityConfig.java
+++ upskillPolicy/src/main/java/com/hex/upskillPolicy/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.hex.upskillPolicy.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,12 +25,20 @@ public class SecurityConfig {
     private final UserService userService;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/customer/sign-up").permitAll()
-                        .anyRequest().authenticated()
-                ).httpBasic();
+    public SecurityFilterChain securedFilterChain(HttpSecurity http) throws Exception {
+        http
+        .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                		.requestMatchers("/api/customer/sign-up").permitAll()
+                		.requestMatchers("/api/policy/add/{companyId}").permitAll()
+                		.requestMatchers("/api/customer-policy/purchase").permitAll()
+                		
+                       
+//                        .anyRequest().authenticated()
+                        
+                        .anyRequest().denyAll()
+                );
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
@@ -36,9 +48,12 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authManager(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userService);
-        provider.setPasswordEncoder(getEncoder());
-        return new ProviderManager(provider);
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userService);
+        authenticationProvider.setPasswordEncoder(getEncoder());
+
+        return new ProviderManager(authenticationProvider);
     }
 }
